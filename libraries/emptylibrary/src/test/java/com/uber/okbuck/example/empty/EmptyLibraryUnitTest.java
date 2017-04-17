cee129e66a56564fceb53a3237f9fac7898f63f5
Chris Woodward <cwoodward@squareup.com>
2017-04-17 16:45:01 -0700

    Try using ComponentName.

--- libraries/emptylibrary/src/test/java/com/uber/okbuck/example/empty/EmptyLibraryUnitTest.java
+++ libraries/emptylibrary/src/test/java/com/uber/okbuck/example/empty/EmptyLibraryUnitTest.java
@@ -1,6 +1,7 @@
 package com.uber.okbuck.example.empty;
 
 import android.content.Context;
+import android.content.ComponentName;
 
 import com.uber.okbuck.BuckRobolectricTestRunner;
 
@@ -17,6 +18,9 @@ import static org.junit.Assert.assertTrue;
 @RunWith(BuckRobolectricTestRunner.class)
 @Config(sdk = 21, packageName = "com.uber.okbuck.example.empty")
 public class EmptyLibraryUnitTest {
+	private static final String PACKAGE_NAME = "com.test";
+    private static final ComponentName CALLING_ACTIVITY =
+       new ComponentName(PACKAGE_NAME, "com.test.TestActivity");
 
     @Test
     public void addition_isCorrect() throws Exception {
